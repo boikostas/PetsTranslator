419f9a691cfd2f29a88072c016eb1af859140a79
boikostas <boikostas3@gmail.com>
2025-03-21 21:07:49 +0200

    project created

---
+++ PetsTranslator/PetsTranslator/PetsTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsTranslatorApp.swift
+//  PetsTranslator
+//
+//  Created by Stas Boiko on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct PetsTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
