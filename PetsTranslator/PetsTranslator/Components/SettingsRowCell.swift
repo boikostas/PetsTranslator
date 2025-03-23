79e6901af6fa7cc3da7c6a4b9bc09d37d35c3d02
boikostas <boikostas3@gmail.com>
2025-03-23 21:15:14 +0200

    finished Settings screen

---
+++ PetsTranslator/PetsTranslator/Components/SettingsRowCell.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsRowCell.swift
+//  PetsTranslator
+//
+//  Created by Stas Boiko on 3/23/25.
+//
+
+import SwiftUI
+
+struct SettingsRowCell: View {
+    
+    var rowType: SettingsRowType
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.purpleTileBackground)
+            HStack {
+                Text(rowType.rawValue)
+                    .font(.customMedium)
+                Spacer()
+                Image(.arrowRightIcon)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .foregroundStyle(.appTint)
+            .padding(.horizontal, 16)
+        }
+        .frame(height: 50)
+    }
+}
+
+#Preview {
+    SettingsRowCell(rowType: .rateUs)
+}
--- PetsTranslator/PetsTranslator/Screens/SettingsScreenView.swift
+++ PetsTranslator/PetsTranslator/Screens/SettingsScreenView.swift
@@ -8,15 +8,78 @@
 import SwiftUI
 
 struct SettingsScreenView: View {
+    
+    @State private var sheetToShow: SettingsRowType?
+    
     var body: some View {
-        VStack {
-            Text(Tab.settings.title)
-                .font(.customTitle)
-                .foregroundStyle(.appTint)
-            
+        VStack(spacing: 12) {
+            title
+            rowsSection
             Spacer()
         }
         .padding(.vertical, 12)
+        .padding(.horizontal, 20)
+        .sheet(item: $sheetToShow) {_ in
+            switch sheetToShow {
+            case .rateUs:
+                Text("Rate")
+                    .presentationDetents([.height(100)])
+            case .shareApp:
+                EmptyView()
+            case .contactUs:
+                Text("Contacts")
+                    .presentationDetents([.height(150)])
+            case .restorePurchases:
+                Text("Purchase")
+            case .privacyPolicy:
+                Text("Privacy Policy")
+            case .termsOfUse:
+                Text("Terms of Use")
+            case nil:
+                EmptyView()
+            
+            }
+        }
+    }
+    
+    private var title: some View {
+        Text(Tab.settings.title)
+            .font(.customTitle)
+            .foregroundStyle(.appTint)
+            .frame(height: 60)
+    }
+    
+    private var rowsSection: some View {
+        VStack(spacing: 14) {
+            ForEach(SettingsRowType.allCases) { rowType in
+                if rowType == .shareApp {
+                    if let url = URL(string: "https://github.com/boikostas/PetsTranslator") {
+                        ShareLink(item: url) {
+                            SettingsRowCell(rowType: rowType)
+                        }
+                    }
+                } else {
+                    Button {
+                        switch rowType {
+                        case .rateUs:
+                            sheetToShow = .rateUs
+                        case .shareApp:
+                            break
+                        case .contactUs:
+                            sheetToShow = .contactUs
+                        case .restorePurchases:
+                            sheetToShow = .restorePurchases
+                        case .privacyPolicy:
+                            sheetToShow = .privacyPolicy
+                        case .termsOfUse:
+                            sheetToShow = .termsOfUse
+                        }
+                    } label: {
+                        SettingsRowCell(rowType: rowType)
+                    }
+                }
+            }
+        }
     }
 }
 
